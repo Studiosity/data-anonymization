498eeabb62b66806b9668e49bfd8fe704732b889
Andrew Bromwich <a.bromwich@gmail.com>
2017-06-09 17:02:29 +1000

    Only persist dirty records

--- lib/strategy/blacklist.rb
+++ lib/strategy/blacklist.rb
@@ -3,15 +3,19 @@ module DataAnon
     class Blacklist < DataAnon::Strategy::Base
 
       def process_record index, record
+        dirty = false
+
         @fields.each do |field, strategy|
           database_field_name = record.attributes.select { |k,v| k == field }.keys[0]
           field_value = record.attributes[database_field_name]
           unless field_value.nil? || is_primary_key?(database_field_name)
             field = DataAnon::Core::Field.new(database_field_name, field_value, index, record, @name)
             record[database_field_name] = strategy.anonymize(field)
+            dirty = true
           end
         end
-        record.save!
+
+        record.save! if dirty
       end
 
     end
--- lib/strategy/whitelist.rb
+++ lib/strategy/whitelist.rb
@@ -7,14 +7,18 @@ module DataAnon
       end
 
       def process_record(index, record)
+        dirty = false
+        
         record.attributes.each do |field_name, field_value|
           unless field_value.nil? || is_primary_key?(field_name)
             field = DataAnon::Core::Field.new(field_name, field_value, index, record, @name)
             field_strategy = @fields[field_name] || default_strategy(field_name)
             record[field_name] = field_strategy.anonymize(field)
+            dirty = true
           end
         end
-        record.save!
+
+        record.save! if dirty
       end
 
     end
